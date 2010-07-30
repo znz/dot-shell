5f4e720adc03973a37d5b6c19cfb38f01a03db33
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-07-30 18:25:14 +0900

    remove export from HISTCONTROL

--- profile.d/50history.bash
+++ profile.d/50history.bash
@@ -2,9 +2,9 @@
 
 # don't put duplicate lines in the history. See bash(1) for more options
 # don't overwrite GNU Midnight Commander's setting of `ignorespace'.
-export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
+HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 # ... or force ignoredups and ignorespace
-export HISTCONTROL=ignoreboth
+HISTCONTROL=ignoreboth
 
 # append to the history file, don't overwrite it
 shopt -s histappend
