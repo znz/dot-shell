642329792470a5f444ccb5365bfe8b38ded55174
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-07-30 18:34:04 +0900

    load *.bash and *.sh order by filenames instead of *.bash after *.sh

--- bashrc.bash
+++ bashrc.bash
@@ -1,7 +1,12 @@
 my_reload () {
     local f
-    for f in ${BASH_SOURCE[0]%/*}/profile.d/*.{,ba}sh; do
-	. $f
+    for f in ${BASH_SOURCE[0]%/*}/profile.d/*sh; do
+	case "$f" in
+	    *.sh|*.bash)
+		echo load $f
+		. $f
+		;;
+	esac
     done
 }
 
