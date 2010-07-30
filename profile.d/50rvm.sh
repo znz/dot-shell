d0c1dbbefd11d24cf2284efcb9bcda98e85af0bd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-07-30 18:02:08 +0900

    use rvm only if bash or zsh

--- profile.d/50rvm.sh
+++ profile.d/50rvm.sh
@@ -1,3 +1,5 @@
-if [[ -s "$HOME/.rvm/scripts/rvm" ]]  ; then
-    . "$HOME/.rvm/scripts/rvm"
+if [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ]; then
+    if [[ -s "$HOME/.rvm/scripts/rvm" ]]  ; then
+	. "$HOME/.rvm/scripts/rvm"
+    fi
 fi
