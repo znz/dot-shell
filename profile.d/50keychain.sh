8c1676ef635a2600ea6fe7f96adaba7041b008b9
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-07-30 18:01:21 +0900

    import my_init_keychain

--- profile.d/50keychain.sh
+++ profile.d/50keychain.sh
@@ -25,6 +25,25 @@ my_keychain_env () {
     done
 }
 
-if type keychain >/dev/null 2>&1; then
+my_init_keychain () {
+    if is_cygwin; then
+        if ps | grep win-ssh-agent >/dev/null 2>&1; then
+            return
+        fi
+    fi
+    if [ -n "$SSH_AGENT_PID" ]; then
+        if kill -0 "$SSH_AGENT_PID" >/dev/null 2>&1; then
+            return
+        fi
+    fi
+    find /tmp -maxdepth 1 -name ssh-\* -type d -uid "`id -u`" -perm 700 -exec /bin/rm -rfv -- '{}' \;
+    find "$HOME/.ssh" -name "*id_[dr]sa" | xargs -t keychain
+    create_cachedir_tag keychain "$HOME/.keychain"
     my_keychain_env
+}
+
+if type keychain >/dev/null 2>&1; then
+    if [ -f "${HOME}/.ssh/id_dsa" -o -f "${HOME}/.ssh/id_rsa" ]; then
+        my_init_keychain
+    fi
 fi
