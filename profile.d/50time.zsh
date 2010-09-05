9fc42e31561696b241cc704dce0bc97dc59093ff
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-09-06 05:49:26 +0900

    set TIMEFMT

---
+++ profile.d/50time.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# see zshparam(1)
+TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P\nmem\t%X+%Dk %Fpf+%Ww\nIO\t%I+%O\ncmd\t%J' 
+TIMEFMT='real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J'
+
