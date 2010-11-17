bd3464d23f79a139f1ec381fa704e032da8717ec
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-11-17 16:02:14 +0900

    add DIRSTACKSIZE

---
+++ profile.d/50dirstack.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# 履歴関係
+DIRSTACKSIZE=50
+setopt AUTO_CD                  # ディレクトリ名でcdできる。
+setopt AUTO_PUSHD               # cd に pushd のような振る舞いをさせる。
+unsetopt CDABLE_VARS            # foo=/tmp;cd fooで/tmpにcdできる。
+setopt PUSHD_IGNORE_DUPS
+setopt PUSHD_MINUS              # +と-の意味を入れ替える。
+setopt PUSHD_SILENT             # pushdやpopdの度にdirstackを表示しない。
+setopt PUSHD_TO_HOME            # 引数無しのpushdをpushd $HOMEと同じようにする。
+
+dirs ${(@u)$(pgrep -u $UID zsh | xargs -iPID readlink "/proc/PID/cwd")}
