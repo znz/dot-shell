b3c3e499bce534f77f98542d9bb88064e7007354
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-12-14 01:38:11 +0900

    HISTCONTROL is colon separated

--- profile.d/50history.bash
+++ profile.d/50history.bash
@@ -2,8 +2,9 @@
 
 # don't put duplicate lines in the history. See bash(1) for more options
 # don't overwrite GNU Midnight Commander's setting of `ignorespace'.
-HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
+#HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 # ... or force ignoredups and ignorespace
+#HISTCONTROL=ignoredups:ignorespace
 HISTCONTROL=ignoreboth
 
 # append to the history file, don't overwrite it
