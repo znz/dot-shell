8481a463e51625469ff89213451429f35bc71985
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-03-14 01:38:17 +0900

    git config --global

---
+++ git-config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+set -ex
+git config --global user.name "$DEBFULLNAME"
+git config --global user.email "$DEBEMAIL"
+git config --global core.excludesfile "$(pwd)/gitignore"
+#git config --global color.branch auto
+#git config --global color.diff auto
+#git config --global color.interactive auto
+#git config --global color.status auto
+git config --global color.ui auto
+git config --global svn.rmdir true
