95d2f76140622903dfb5ee61b5b0098ef05813e3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-04-02 16:34:20 +0900

    rbenv and rvm make lazy loading

--- profile.d/50rbenv.sh
+++ profile.d/50rbenv.sh
@@ -1,8 +1,20 @@
-if [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ]; then
-    if [ -d "$HOME/.rbenv/bin" ]; then
-        PATH="$HOME/.rbenv/bin:$PATH"
+# lazy loading
+rbenv () {
+    if [ -n "$rvm_path" ]; then
+	echo "rvm is enabled."
+	return 1
     fi
-    if type rbenv >/dev/null 2>&1; then
-	eval "$(rbenv init -)"
+    if [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ]; then
+	if [ -d "$HOME/.rbenv/bin" ]; then
+	    PATH="$HOME/.rbenv/bin:$PATH"
+	fi
+	unset -f rbenv
+	if type rbenv >/dev/null 2>&1; then
+	    eval "$(rbenv init -)"
+	fi
+	rbenv "$@"
+    else
+	echo "$SHELL is not supported."
+	return 1
     fi
-fi
+}
--- profile.d/50rvm.sh
+++ profile.d/50rvm.sh
@@ -1,5 +1,21 @@
-if [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ]; then
-    if [[ -s "$HOME/.rvm/scripts/rvm" ]]  ; then
-	. "$HOME/.rvm/scripts/rvm"
+# lazy loading
+rvm () {
+    case "$(type ruby)" in
+	*rbenv*)
+	    echo "rbenv is enabled."
+	    return 1
+	    ;;
+        *)
+	    ;;
+    esac
+    if [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ]; then
+	unset -f rvm
+	if [[ -s "$HOME/.rvm/scripts/rvm" ]]  ; then
+	    . "$HOME/.rvm/scripts/rvm"
+	fi
+	rvm "$@"
+    else
+	echo "$SHELL is not supported."
+	return 1
     fi
-fi
+}
