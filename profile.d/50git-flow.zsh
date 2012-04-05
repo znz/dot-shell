f4e33cfcdccac6ed1a96930e7d6a608d6bc13728
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-04-05 17:51:11 +0900

    add git-flow-completion.zsh in Homebrew

--- profile.d/50git-flow.zsh
+++ profile.d/50git-flow.zsh
@@ -2,3 +2,7 @@
 if [ -f /usr/share/git-flow/git-flow-completion.zsh ]; then
     source /usr/share/git-flow/git-flow-completion.zsh
 fi
+# Homebrew
+if [ -f /usr/local/share/zsh/functions/git-flow-completion.zsh ]; then
+    source /usr/local/share/zsh/functions/git-flow-completion.zsh
+fi
