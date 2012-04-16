f65779a8dbe6058415b64b0ff722d483db28a8a2
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-04-16 22:11:05 +0900

    use function instead of alias to avoid error when ssh with command

--- profile.d/10cygwin.sh
+++ profile.d/10cygwin.sh
@@ -2,13 +2,21 @@
 
 case `uname` in
     CYGWIN_*)
-        alias is_cygwin=:
-        alias is_not_cygwin=false
+        is_cygwin () {
+            :
+        }
+        is_not_cygwin () {
+            false
+        }
         # export MAKE_MODE=unix
         # unset TEMP TMP
         ;;
     *)
-        alias is_cygwin=false
-        alias is_not_cygwin=:
+        is_cygwin () {
+            false
+        }
+        is_not_cygwin () {
+            :
+        }
         ;;
 esac
