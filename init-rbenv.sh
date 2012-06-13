97af61e6d1ff0529f8cb3c4eaf95437988fc3595
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2012-06-14 07:56:08 +0900

    support system rbenv

--- init-rbenv.sh
+++ init-rbenv.sh
@@ -5,7 +5,9 @@ set -ex
 cd
 if [ -d .rbenv ]; then
     cd .rbenv
-    git pull
+    if [ -d .git ]; then
+        git pull
+    fi
 else
     git clone git://github.com/sstephenson/rbenv.git .rbenv
 fi
