8c877658baab709fa6cd1190841577ab333a9ab3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-02-25 11:36:31 +0900

    rbenv-alternatives

--- init-rbenv.sh
+++ init-rbenv.sh
@@ -18,6 +18,17 @@ cd .rbenv/plugins
 if [ -d ruby-build ]; then
     cd ruby-build
     git pull
+    cd ..
 else
     git clone git://github.com/sstephenson/ruby-build.git
+fi
+
+if [ -L /etc/alternatives/ruby ]; then
+    if [ -d rbenv-alternatives ]; then
+        cd rbenv-alternatives
+        git pull
+        cd ..
+    else
+        git clone git://github.com/terceiro/rbenv-alternatives.git
+    fi
 fi
