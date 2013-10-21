ee39b2aae840f89bed3769f3e258c18816c099f7
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-21 11:38:13 +0900

    curl-ca-bundle, git-flow, emacs, ricty

--- init-brew.rb
+++ init-brew.rb
@@ -20,17 +20,23 @@ unless installed?('brew')
   xsystem(%W"git clone https://github.com/mxcl/homebrew.git #{PREFIX}")
 end
 
+unless (PREFIX + "opt/curl-ca-bundle/share/ca-bundle.crt").exist?
+    xsystem(%W"brew install curl-ca-bundle")
+end
+
 commands = {
   'lv' => 'lv',
   'git' => 'git',
+  'git-flow' => 'git-flow',
   'wget' => 'wget',
   'cocot' => 'cocot',
   'mosh' => 'mobile-shell',
   '7z' => 'p7zip',
+  'emacs' => %w'emacs --cocoa --srgb --with-gnutls',
 }
-commands.each do |cmd, formula|
+commands.each do |cmd, *formula|
   unless installed?(cmd)
-    xsystem(%W"brew install #{formula}")
+    xsystem(%W"brew install" + formula.flatten)
   end
 end
 
@@ -41,3 +47,18 @@ end
 #unless installed?('emacs')
 #  xsystem(%W"env CC=gcc-4.2 CXX=g++-4.2 brew install emacs --cocoa")
 #end
+
+if Dir.glob(File.expand_path("~/Library/Fonts/Ricty*.ttf")).empty?
+  unless /^sanemat\/font$/ =~ `brew tap`
+    xsystem(%W"brew tap sanemat/font")
+  end
+  xsystem(%W"brew install ricty")
+  require 'fileutils'
+  FileUtils::Verbose.cp(Dir.glob(PREFIX + "Cellar/ricty/*/share/fonts/Ricty*.ttf"), File.expand_path("~/Library/Fonts/"))
+  xsystem(%W"fc-cache -vf")
+  #xsystem(%W"defaults -currentHost write -globalDomain AppleFontSmoothing -int 2")
+end
+
+# Emacs
+xsystem("brew linkapps")
+# or ln -s /Users/kazu/homebrew/Cellar/emacs/24.3/Emacs.app /Applications
