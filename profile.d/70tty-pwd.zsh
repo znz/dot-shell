72309f25ab09ca1c4f9c2f012d35c928627b695a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-22 16:58:42 +0900

    tty may include /

--- profile.d/70tty-pwd.zsh
+++ profile.d/70tty-pwd.zsh
@@ -1,12 +1,17 @@
+tty_pwd_file () {
+    echo "${XDG_CACHE_HOME:-$HOME/.cache}/shell/${$(print -P '%l')/\//-}.pwd"
+}
+
 tty_pwd_restore () {
-    local tty_pwd="${XDG_CACHE_HOME:-$HOME/.cache}/shell/$(print -P '%l')".pwd
+    local tty_pwd="$(tty_pwd_file)"
     if [ -f "$tty_pwd" ]; then
         cd $(<"$tty_pwd")
     fi
 }
 tty_pwd_restore
+
 tty_pwd_save () {
-    local tty_pwd="${XDG_CACHE_HOME:-$HOME/.cache}/shell/$(print -P '%l')".pwd
+    local tty_pwd="$(tty_pwd_file)"
     pwd >"$tty_pwd"
 }
 chpwd_functions+=tty_pwd_save
