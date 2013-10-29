671032b914f990fc3a3c26ffaa447f0448ccdae0
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-10-29 11:37:59 +0900

    set fpath relative from brew

--- profile.d/50homebrew.zsh
+++ profile.d/50homebrew.zsh
@@ -1,3 +1,5 @@
-fpath=($fpath $HOME/homebrew/share/zsh/site-functions(N))
+# when homebrew installed to $HOME/homebrew
+#fpath=($fpath $HOME/homebrew/share/zsh/site-functions(N))
+fpath=($fpath ${commands[brew]:h:h}/share/zsh/site-functions(N))
 # brew install zsh-completions
-fpath=(/usr/local/share/zsh-completions(N) $fpath)
+fpath=(${commands[brew]:h:h}/share/zsh-completions(N) $fpath)
