d5e63c805ef707e67762c951371efecfba1a80f1
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-12 01:31:00 +0900

    add duplicated mkdir -p into comment

--- profile.d/70cdr.zsh
+++ profile.d/70cdr.zsh
@@ -8,6 +8,7 @@ if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]
   zstyle ':completion:*' recent-dirs-insert both
   zstyle ':chpwd:*' recent-dirs-max 500
   zstyle ':chpwd:*' recent-dirs-default true
+  #mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}/shell"
   zstyle ':chpwd:*' recent-dirs-file "${XDG_CACHE_HOME:-$HOME/.cache}/shell/chpwd-recent-dirs"
   zstyle ':chpwd:*' recent-dirs-pushd true
 fi
