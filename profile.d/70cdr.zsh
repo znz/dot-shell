9fce76d6ff9c6c96303179e3934bae7370a85782
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-12 01:06:25 +0900

    add chpwd_recent_dirs and cdr

---
+++ profile.d/70cdr.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# vim:set fileencoding=utf-8:
+
+if [[ -n $(echo ${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ${^fpath}/cdr(N)) ]]; then
+  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+  add-zsh-hook chpwd chpwd_recent_dirs
+  zstyle ':completion:*:*:cdr:*:*' menu selection
+  zstyle ':completion:*' recent-dirs-insert both
+  zstyle ':chpwd:*' recent-dirs-max 500
+  zstyle ':chpwd:*' recent-dirs-default true
+  zstyle ':chpwd:*' recent-dirs-file "${XDG_CACHE_HOME:-$HOME/.cache}/shell/chpwd-recent-dirs"
+  zstyle ':chpwd:*' recent-dirs-pushd true
+fi
+
+# Local Variables:
+# coding: utf-8
+# indent-tabs-mode: nil
+# End:
