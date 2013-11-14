122af86904fe150bcb9471703076a94d223ade72
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-14 16:21:14 +0900

    TIMEFMT with bell

--- profile.d/50time.zsh
+++ profile.d/50time.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 # see zshparam(1)
-TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P\nmem\t%X+%Dk %Fpf+%Ww\nIO\t%I+%O\ncmd\t%J' 
-TIMEFMT='real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J'
+#TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P\nmem\t%X+%Dk %Fpf+%Ww\nIO\t%I+%O\ncmd\t%J'
+#TIMEFMT='real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J'
+TIMEFMT=$'real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J\a'
 
