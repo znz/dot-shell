4b8f9cc9638209e8fb3a58c6436bec57c6918a2e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2013-11-20 10:39:53 +0900

    change TIMEFMT

--- profile.d/50time.zsh
+++ profile.d/50time.zsh
@@ -3,7 +3,7 @@
 # see zshparam(1)
 #TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P\nmem\t%X+%Dk %Fpf+%Ww\nIO\t%I+%O\ncmd\t%J'
 #TIMEFMT='real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J'
-TIMEFMT=$'real:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J\a\a'
+TIMEFMT=$'\areal:%E  user:%U  sys:%S  cpu:%P mem:%X+%Dk %Fpf+%Ww  IO:%I+%O  %J\a'
 
 # 起動したコマンドの「ユーザ消費時間+システム消費時間」が $REPORTTIME 秒よりも
 # 大きいときに time 内部コマンドを指定したのと同じ情報が表示される。
