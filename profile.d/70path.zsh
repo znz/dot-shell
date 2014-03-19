e08a2e249f076437223127cc6fe9491775d38137
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-03-19 13:30:57 +0900

    add ./bin to path if safe

--- profile.d/70path.zsh
+++ profile.d/70path.zsh
@@ -8,3 +8,14 @@ path=(
     # reject world-writable directories (^W)
     $path(N-/^W)
 )
+
+bin_to_path () {
+    # ./bin should be owned by me, and not group-writable nor world-writable.
+    # ./bin/* should not be either.
+    if [ -d "$(echo bin(Nu$UID-^IW))" -a -z "$(echo bin/*(NxI,xW))" ]; then
+        path=(bin ${path:#bin})
+    else
+        path=(${path:#bin})
+    fi
+}
+chpwd_functions+=bin_to_path
