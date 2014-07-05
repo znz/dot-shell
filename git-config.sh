bb84c5aefc83eab5ce1841508abd726ee9db6577
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-05 09:39:28 +0900

    add ghq config

--- git-config.sh
+++ git-config.sh
@@ -13,11 +13,21 @@ git config --global svn.rmdir true
 #git config --global push.default simple
 git config --global push.default tracking
 
+# github upload
 GITHUB_URL_PREFIX="url.git@github.com:"
 git config --global --remove-section "$GITHUB_URL_PREFIX" || :
 git config --global       "$GITHUB_URL_PREFIX".pushInsteadOf "git://github.com/"
 git config --global --add "$GITHUB_URL_PREFIX".pushInsteadOf "https://github.com/"
-
+# gist upload
 git config --global "url.git@gist.github.com:".pushInsteadOf "https://gist.github.com/$(git config github.user)/"
+# github download
+git config --global url."git://github.com/".insteadOf "https://github.com/"
+
+# ghq section
+git config --global --remove-section "ghq" || :
+GHQ_ROOT="ghq.root"
+#git config --global --unset-all "$GHQ_ROOT" || :
+git config --global       "$GHQ_ROOT" "$HOME/s"
+git config --global --add "$GHQ_ROOT" "$HOME/g/src"
 
 git config --global init.templatedir "$(pwd)/git-templates"
