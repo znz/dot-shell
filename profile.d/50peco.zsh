e29766179ce3f41b59e3e4abc1bc53ade8aad48e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-11 17:45:03 +0900

    Add peco-find-file and peco-select-history
    see http://k0kubun.hatenablog.com/entry/2014/07/06/033336

--- profile.d/50peco.zsh
+++ profile.d/50peco.zsh
@@ -1,4 +1,19 @@
 if (( ${+commands[peco]} )); then
+  peco-find-file () {
+    local selected_files
+    selected_files="$(if git rev-parse 2>/dev/null; then
+      git ls-files
+    else
+      find . -type f
+    fi | peco --prompt '[find file]')"
+    selected_files=("${(qf)selected_files}")
+    BUFFER="${BUFFER}${selected_files}"
+    CURSOR=$#BUFFER
+    zle redisplay
+  }
+  zle -N peco-find-file
+  bindkey '^[F' peco-find-file
+
   peco-go-to-dir () {
     local line
     local selected="$(
@@ -23,6 +38,14 @@ if (( ${+commands[peco]} )); then
   }
   zle -N peco-go-to-dir
   bindkey '^[g' peco-go-to-dir
+
+  peco-select-history () {
+    BUFFER=$(fc -l -r -n 1 | peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle redisplay
+  }
+  zle -N peco-select-history
+  bindkey '^[r' peco-select-history
 
   # http://qiita.com/d6rkaiz/items/46e9c61c412c89e84c38#comment-374d8378994ca64ddad7
   #alias s='ssh $(awk 'tolower($1)=="host"{$1="";print}' ~/.ssh/config | xargs -n1 | egrep -v '[*?]' | sort -u | peco)'
