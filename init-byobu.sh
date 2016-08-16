8d40d768f5ed2901f4e94038bddc1e5b450b5930
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-17 01:36:41 +0900

    Initialize byobu

---
+++ init-byobu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+install -d "$HOME/.byobu"
+cat <<EOF >"$HOME/.byobu/keybindings.tmux"
+unbind-key -n C-a
+unbind-key -n C-z
+set -g prefix ^Z
+set -g prefix2 ^Z
+bind z send-prefix
+EOF
+SHELL=/bin/true byobu
+sed -i -e 's/\([^#]\)logo/\1#logo/g' "$HOME/.byobu/status"
+echo 'SHELL=/bin/zsh byobu' >>"$HOME/.bash_history"
