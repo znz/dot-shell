680d42005545640fa4b92db19bfc598b5048993c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-08-06 18:59:59 +0900

    Ignore .git too

--- profile.d/60completion.zsh
+++ profile.d/60completion.zsh
@@ -25,7 +25,8 @@ fi
 # 補完候補から.svnを除く。
 # .で始まるディレクトリが候補になっていなかったのでコメントアウトしていたが、
 # .で始まる補完の場合に出ていたので有効に。
-zstyle ':completion:*:*:*:*' ignored-patterns '.svn'
+# .gitignore などの保管に邪魔なので .git も追加。
+zstyle ':completion:*:*:*:*' ignored-patterns '.(svn|git)'
 
 # Prevent CVS files/directories from being completed:
 #zstyle ':completion:*:(all-|)files' ignored-patterns '(|*/)CVS'
