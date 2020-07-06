9aecb33f949778cc0b50d3ebec77ad1d8413037b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-07-06 16:26:28 +0900

    Docker.app does not contain docker-machine.zsh-completion now

--- init-zsh-completion-brew-docker.sh
+++ init-zsh-completion-brew-docker.sh
@@ -2,4 +2,4 @@
 set -eux
 ln -sf /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
 ln -sf /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
-ln -sf /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
+#ln -sf /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
