2e67b25a39065297d7d797bfaf787437e09d661b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2022-04-17 20:52:05 +0900

    Add org.gpgtools.common UseKeychain NO

--- defaults.sh
+++ defaults.sh
@@ -3,3 +3,5 @@ set -ex
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 2
 # http://support.apple.com/kb/HT1629?viewlocale=ja_JP
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+defaults write org.gpgtools.common UseKeychain NO
