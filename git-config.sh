ddb11384b90816bd6c1e9c565b0b41e526b06cc6
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2025-04-11 09:44:02 +0900

    Update git config

--- git-config.sh
+++ git-config.sh
@@ -10,11 +10,32 @@ git config --global core.excludesfile "$(pwd)/gitignore"
 #git config --global color.status auto
 git config --global color.ui auto
 git config --global svn.rmdir true
-if [ "$(lsb_release -cs)" = "precise" ]; then
-    git config --global push.default tracking
-else
-    git config --global push.default simple
-fi
+git config --global push.default simple
+
+# https://blog.gitbutler.com/how-git-core-devs-configure-git/
+git config --global column.ui auto
+git config --global branch.sort -committerdate
+git config --global tag.sort version:refname
+#git config --global init.defaultBranch main
+git config --global diff.algorithm histogram
+git config --global diff.colorMoved plain
+git config --global diff.mnemonicPrefix true
+git config --global diff.renames true
+#git config --global fetch.prune true
+#git config --global fetch.pruneTags true
+#git config --global fetch.all true
+#git config --global help.autocorrect prompt
+git config --global commit.verbose true
+#git config --global rerere.enabled true
+#git config --global rerere.autoupdate true
+#git config --global core.excludesfile ~/.gitignore
+git config --global rebase.autoSquash true
+git config --global rebase.autoStash true
+git config --global rebase.updateRefs true
+git config --global merge.conflictstyle zdiff3
+git config --global pull.rebase true
+#git config --global core.fsmonitor true
+#git config --global core.untrackedCache true
 
 # github upload
 GITHUB_URL_PREFIX="url.git@github.com:"
