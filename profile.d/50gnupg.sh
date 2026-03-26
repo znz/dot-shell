9183ab2de02ad783f0a9ee5b5e28fe379438e5d6
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2026-03-26 14:48:59 +0900

    Move updating gpg-agent.conf to init.sh

--- profile.d/50gnupg.sh
+++ profile.d/50gnupg.sh
@@ -1,11 +1,3 @@
-if [ ! -d ~/.gnupg ]; then
+if [ -d "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}" ]; then
     export GNUPGHOME="${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
-fi
-
-if [ -x /opt/homebrew/bin/pinentry-mac ]; then
-    if ! grep -q "pinentry-program /opt/homebrew/bin/pinentry-mac" "${GNUPGHOME:-"${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"}/gpg-agent.conf"; then
-	install -m 700 -d "${GNUPGHOME:-"${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"}"
-	echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> "${GNUPGHOME:-"${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"}/gpg-agent.conf"
-	defaults write org.gpgtools.common UseKeychain NO
-    fi
 fi
