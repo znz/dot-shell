d9d7ff97180e1ff56edd30eef60615fbe1b53924
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2026-03-26 15:01:13 +0900

    untabify

--- init.sh
+++ init.sh
@@ -24,11 +24,11 @@ set -euo pipefail
 () { # (Re-)start gpg-agent
     gpgconf --kill gpg-agent
     if [ -x /opt/homebrew/bin/pinentry-mac ]; then
-	if ! grep -q "pinentry-program /opt/homebrew/bin/pinentry-mac" "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}/gpg-agent.conf"; then
-	    install -m 700 -d "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}"
-	    echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}/gpg-agent.conf"
-	    defaults write org.gpgtools.common UseKeychain NO
-	fi
+        if ! grep -q "pinentry-program /opt/homebrew/bin/pinentry-mac" "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}/gpg-agent.conf"; then
+            install -m 700 -d "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}"
+            echo "pinentry-program /opt/homebrew/bin/pinentry-mac" >> "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}/gpg-agent.conf"
+            defaults write org.gpgtools.common UseKeychain NO
+        fi
     fi
     gpg -K &>/dev/null
 }
